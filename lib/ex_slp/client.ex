4913b844eb5f5b1741d1fe85c1b42f6727003271
4pcbr <me@4pcbr.com>
2016-02-24 22:50:53 +0100

    Implemented findsrvs functionality

--- lib/ex_slp/client.ex
+++ lib/ex_slp/client.ex
@@ -1,10 +1,16 @@
 defmodule ExSlp.Client do
 
   alias ExSlp.Tool
-  alias ExSlp.Util
+  import ExSlp.Util, only: [ format_servise_url: 1, format_args: 1, format_opts: 1 ]
 
-  def findsrvs( service, args, filter ) do
-    #case res = Tool.exec_cmd( args, :findsrvs, [ format_servise_url( service ), filter ])
+  def findsrvs( service ), do: findsrvs( service, [], [] )
+
+  def findsrvs( service, opts ), do: findsrvs( service, [], opts )
+
+  def findsrvs( service, args, opts ) do
+    args = format_args( args )
+    opts = format_opts( opts )
+    Tool.exec_cmd( args, :findsrvs, [ format_servise_url( service ), opts ])
   end
 
 end
--- test/ex_slp_client_test.exs
+++ test/ex_slp_client_test.exs
@@ -2,6 +2,34 @@ defmodule ExSlpClientTest do
   use ExUnit.Case
   import ExSlp.Client
 
+  alias ExSlp.Server
+
+  test "Should find a registered service with no search filters" do
+    assert { :ok, _ } = Server.status
+    service_type = "myservice"
+    service = "#{service_type}://localhost"
+    args = [ u: "localhost" ]
+    expected_res = "service:#{service},65535"
+    assert { :ok, _ } = Server.register( service )
+    assert { :ok, real_res } = findsrvs( service_type, args, [] )
+    assert expected_res == real_res
+    assert { :ok, _ } = Server.deregister( service )
+  end
+
+  @tag skip: "slptool sometimes fails to find a service with a given attribute"
+  test "Should find a registered service with search filters" do
+    assert { :ok, _ } = Server.status
+    service_type = "myservice"
+    service = "#{service_type}://localhost"
+    expected_res = "service:#{service},65535"
+    args = [ u: "localhost" ]
+    opts = [ v: 1.01 ]
+    assert { :ok, _ } = Server.register( service, [], opts )
+    assert { :ok, real_res } = findsrvs( service_type, args, opts )
+    assert expected_res == real_res
+    assert { :ok, _ } = Server.deregister( service )
+  end
+
 
 end
 
