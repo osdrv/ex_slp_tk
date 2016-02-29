c3d47683188e1ccc1da46286100699a869dface2
4pcbr <me@4pcbr.com>
2016-02-29 09:10:02 +0100

    Completed the missing hex package attributes

--- mix.exs
+++ mix.exs
@@ -3,12 +3,14 @@ defmodule ExSlp.Mixfile do
 
   def project do
     [
-      app: :ex_slp,
-      version: "0.0.1",
-      elixir: "~> 1.2",
-      build_embedded: Mix.env == :prod,
+      app:             :ex_slp,
+      version:         "0.0.1",
+      elixir:          "~> 1.2",
+      build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps
+      deps:            deps,
+      description:     description,
+      package:         package,
    ]
   end
 
@@ -24,4 +26,24 @@ defmodule ExSlp.Mixfile do
       { :earmark, ">= 0.0.0" },
     ]
   end
+
+  defp description do
+    """
+    Zero-config local network Elixir/Erlang node discovery lib.
+    Allows an Elixir node to register itself as a local netowrk service and discover the orher registered services.
+    """
+  end
+
+
+  defp package do
+    [
+      files:       ~w/lib mix.exs README* LICENSE*/,
+      licenses:    ~w/MIT/,
+      maintainers: [ "Oleg S" ],
+      links: %{
+        "GitHub" => "https://github.com/4pcbr/ex_slp_tk",
+      },
+    ]
+  end
+
 end
