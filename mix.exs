91a9f435cdc62224219409b9560a031d5ff4bb39
icanhazbroccoli <me@4pcbr.com>
2017-08-19 17:29:54 +0200

    Regenerated the package documentation

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ExSlp.Mixfile do
   def project do
     [
       app:             :ex_slp,
-      version:         "0.1.1",
-      elixir:          "~> 1.1",
+      version:         "0.1.2",
+      elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps:            deps(),
